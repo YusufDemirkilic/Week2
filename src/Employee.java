6dafda3f41e5bd735657b364fee74abf6a18d84f
Yusuf Demirkılıç <yusuf.demirkilic1@gmail.com>
2024-03-03 01:19:42 +0300

    Employee version finishupload

--- src/Employee.java
+++ src/Employee.java
@@ -1,38 +1,73 @@
-import java.sql.SQLOutput;
 
 public class Employee {
-    String Name_surname;
+    String name_surname;
     double salary;
     int weekylWorkHourse;
     int hireYear;
+    double totoal = salary;
 
     public Employee(String name_surname, double salary, int weekylWorkHourse, int hireYear) {
-        Name_surname = name_surname;
+        this.name_surname = name_surname;
         this.salary = salary;
         this.weekylWorkHourse = weekylWorkHourse;
         this.hireYear = hireYear;
-    }
-    public void tax(){
+        System.out.println(toString());
+        tax();
+        bonus();
+        raiseSalary();
 
-        if (salary<1000){
-            System.out.println("Salary: "+salary);
-        }
-        if (1000<=salary){
-            System.out.println("Salary: "+(salary+(salary*0.03)));
+    }
+
+    // vergi hesaplama
+    public void tax() {
+        if (1000 <= this.salary) {
+            System.out.println("tax: " + salary * 0.03);
+            this.totoal = salary - (salary * 0.03);
+
         }
     }
-    public void bonus(int weekylWorkHourse){
-        boolean control=true;
-        while(control){
-            if (weekylWorkHourse==40){
-                control=false;
+// fazla mesai
+    public void bonus() {
+        int bonus = 0;
 
-            }
-            else {
-                salary+=salary+30;
-                weekylWorkHourse--;
-            }
+        if (this.weekylWorkHourse == 40) {
+            System.out.println("salary bonus: " + bonus);
+
+        } else {
+            bonus = (this.weekylWorkHourse - 40) * 30;
+            this.totoal += bonus;
+            System.out.println("salary bonus: " + bonus);
         }
+
+    }
+
+//kıdem zammı
+    public void raiseSalary() {
+        int thisyear = 2021 - this.hireYear;
+        double salary = this.salary;
+
+        if (thisyear < 10) {
+            this.salary = salary + (salary * 0.05);
+            System.out.println("Salary increase: " + salary * 0.05);
+            this.totoal += salary * 0.05;
+        }
+        if (10 < thisyear && thisyear < 20) {
+            this.salary = salary + (salary * 0.10);
+            System.out.println("Salary increase: " + salary * 0.10);
+            this.totoal += salary * 0.10;
+        }
+        if (19 < thisyear) {
+            this.salary = salary + (salary * 0.15);
+            System.out.println("Salary increase: " + salary * 0.15);
+            this.totoal += salary * 0.15;
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Name surname: " + name_surname + "\nSalary: " + salary + "\nWork hours: " + weekylWorkHourse + "\nHire year: " + hireYear;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Employee employee=new Employee("Ahmet mümtaz",1000,45,2019);
+        System.out.println("Total Salary: "+employee.totoal);
 
 
     }
